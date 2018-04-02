7542a57caafa1e269426e63cb7754730b99de6a3
Wito Chandra <wito.c.91@gmail.com>
2018-04-02 22:00:04 +0700

    update example

--- example/main.dart
+++ example/main.dart
@@ -1,11 +1,13 @@
 import 'package:http/http.dart' as http;
-import 'client.dart';
+import 'package:webfeed/domain/rss_feed.dart';
 
 void main() {
-  var client = new WebFeedClient(new http.Client());
-  client.fetch("https://developer.apple.com/news/releases/rss/releases.rss").then((channel) {
+  var client = new http.Client();
+  client.get("https://developer.apple.com/news/releases/rss/releases.rss").then((response) {
+    return response.body;
+  }).then((bodyString) {
+    var channel = new RssFeed.parse(bodyString);
     print(channel);
-  }).catchError((error) {
-    print(error);
+    return channel;
   });
 }
