51b6b5b55570b9656b558adee47909a1b4bcd5aa
Wito Chandra <wito.c.91@gmail.com>
2018-04-06 21:25:48 +0700

    add atom example

--- example/main.dart
+++ example/main.dart
@@ -1,13 +1,25 @@
 import 'package:http/http.dart' as http;
+import 'package:webfeed/domain/atom_feed.dart';
 import 'package:webfeed/domain/rss_feed.dart';
 
 void main() {
   var client = new http.Client();
+
+  // RSS feed
   client.get("https://developer.apple.com/news/releases/rss/releases.rss").then((response) {
     return response.body;
   }).then((bodyString) {
     var channel = new RssFeed.parse(bodyString);
     print(channel);
     return channel;
+  });
+
+  // Atom feed
+  client.get("https://www.theverge.com/rss/index.xml").then((response) {
+    return response.body;
+  }).then((bodyString) {
+    var feed = new AtomFeed.parse(bodyString);
+    print(feed);
+    return feed;
   });
 }
