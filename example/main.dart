7bafc8d0ee9cc7416029ec525ec9384c23d4f4d3
Alex Baker <iamalexbaker@gmail.com>
2021-03-06 15:59:09 +0700

    Updated to use new http and intl versions

--- example/main.dart
+++ example/main.dart
@@ -6,12 +6,13 @@ void main() async {
 
   // RSS feed
   var response = await client
-      .get('https://developer.apple.com/news/releases/rss/releases.rss');
+      .get(Uri.parse('https://developer.apple.com/news/releases/rss/releases.rss'));
   var channel = RssFeed.parse(response.body);
   print(channel);
+  print('${channel.items[0].title}\n-----------\n${channel.items[0].description}');
 
   // Atom feed
-  response = await client.get('https://www.theverge.com/rss/index.xml');
+  response = await client.get(Uri.parse('https://www.theverge.com/rss/index.xml'));
   var feed = AtomFeed.parse(response.body);
   print(feed);
 
