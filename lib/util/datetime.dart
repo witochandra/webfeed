d25f76a4756a7741776d90da8f3f07578a4ac20f
Khlimenkov <xlimenkov4@gmail.com>
2022-08-30 18:36:11 +0300

    fix: formatting

--- lib/util/datetime.dart
+++ lib/util/datetime.dart
@@ -14,9 +14,9 @@ DateTime? _parsePatternDateTime(String dateString, String pattern) {
   try {
     final num? length = dateString.length.clamp(0, pattern.length);
     final trimmedPattern = pattern.substring(
-        0,
-        length
-            as int?); //Some feeds use a shortened RFC 822 date, e.g. 'Tue, 04 Aug 2020'
+      0,
+      length as int?,
+    ); //Some feeds use a shortened RFC 822 date, e.g. 'Tue, 04 Aug 2020'
     final format = DateFormat(trimmedPattern, 'en_US');
     return format.parse(dateString);
   } on FormatException {
